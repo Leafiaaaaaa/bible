95ba3facc6b907eabe939a87a09446660de72f91
Leafiaaaaaa <402204.joyce.10@gmail.com>
2026-03-10 20:47:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-// sw.js - 網路優先模式（不進行任何快取）
-self.addEventListener('install', () => {
-  self.skipWaiting(); // 強制跳過等待，立即更新
-});
 
-self.addEventListener('activate', () => {
-  console.log('PWA 已啟用：網路優先模式');
-});
-
-// 攔截請求，但不做快取，直接去網路上抓
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
